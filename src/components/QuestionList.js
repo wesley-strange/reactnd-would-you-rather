5c8497c4cc5af6b09cd6733f8965c23085fe1001
Wesley Strange <wesley.ws.strange@gmail.com>
2020-08-16 14:15:45 -0700

    QuestionList Toggle Answered / Unanswered

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -6,28 +6,52 @@ class QuestionList extends Component {
     answered: false
   }
 
+  toggleAnswered = (e) => {
+    this.setState((prevState) => ({
+      answered: !prevState.answered
+    }))
+  }
+
   render() {
     const { answered } = this.state
     const { answeredQuestions, unansweredQuestions } = this.props
 
     return (
       <div>
-        <h3 className='center'>Questions</h3>
-        <ul className='dashboard-list'>
-          {answered
-            ? (
-              answeredQuestions.map((question) => (
+        <div>
+          <button
+            name='unanswered'
+            disabled={!answered}
+            onClick={this.toggleAnswered}
+          >
+            Unanswered
+          </button>
+          <button
+            name='answered'
+            disabled={answered}
+            onClick={this.toggleAnswered}
+          >
+            Answered
+          </button>
+        </div>
+        <div>
+          <h3 className='center'>Questions</h3>
+          <ul className='dashboard-list'>
+            {answered
+              ? (
+                answeredQuestions.map((question) => (
+                  <li key={question.id}>
+                    {question.id}
+                  </li>
+                ))
+              )
+              : unansweredQuestions.map((question) => (
                 <li key={question.id}>
                   {question.id}
                 </li>
-              ))
-            )
-            : unansweredQuestions.map((question) => (
-              <li key={question.id}>
-                {question.id}
-              </li>
-            ))}
-        </ul>
+              ))}
+          </ul>
+        </div>
       </div>
     )
   }
